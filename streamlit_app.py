758cf7c875f489ddcba013181550bde441361402
nellyrdz-cellgit <nellyrdz@gmail.com>
2026-02-19 20:21:36 +0000

    release version 1.0

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,10 @@ from google.cloud import firestore
 from google.oauth2 import service_account
 
 import json
+
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds, project="movies")
+db = firestore.Client(credentials=creds, project="movies-199f4")
 dbMovies = db.collection('movies')
 
 # Titulo
