4d5de404f06f2511cf1e3b773e4a076d44a42eb1
nellyrdz-cellgit <nellyrdz@gmail.com>
2026-02-20 05:47:46 +0000

    release version 1.0

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,29 +14,83 @@ creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="movies-199f4")
 dbMovies = db.collection('movies')
 
+@st.cache_data
+def load_movies():
+    docs = dbMovies.stream()
+    data = [doc.to_dict() for doc in docs]
+    return pd.DataFrame(data)
+
+movies_df = load_movies()
+
 # Titulo
 st.title("Lista de Películas")
 
+#Todas las peliculas
+st.sidebar.header("Visualización")
+show_all = st.sidebar.checkbox("Mostrar todos los filmes")
+# Mostrar resultados principales
+if show_all:
+    st.subheader("Listado completo de filmes")
+    st.dataframe(movies_df, use_container_width=True)
+
+elif search_btn and title_search:
+    filtered_df = movies_df[
+        movies_df["name"].str.contains(title_search, case=False, na=False)
+    ]
+
+    st.subheader("Resultados de búsqueda")
+    st.dataframe(filtered_df, use_container_width=True)
+
+#Buscar por titulo
+st.sidebar.header("Buscar por título de filme")
+
+title_search = st.sidebar.text_input("Título del filme")
+search_btn = st.sidebar.button("Buscar")
+
+if search_btn and title_search:
+    filtered_df = movies_df[
+        movies_df["name"].str.contains(title_search, case=False, na=False)
+    ]
+
+    st.subheader("Resultados de búsqueda")
+    st.dataframe(filtered_df, use_container_width=True)
+
+
+#Si escogio ver todos o filtrado
+if show_all:
+    st.subheader("Listado completo de filmes")
+    st.dataframe(movies_df)
+
+elif search_btn and title_search:
+    filtered_df = movies_df[
+        movies_df["name"].str.contains(title_search, case=False, na=False)
+    ]
+
+    st.subheader("Resultados de búsqueda")
+    st.dataframe(filtered_df)
 
 # Crea película
-st.header("Nueva película")
-name = st.text_input('Nombre de la película: ')
-filemcompany = st.text_input('Compañía Cinematográfica: ')
-director = st.text_input('Director: ')
-genre = st.text_input('Género: ')
+#  doc_ref = db.collection('movies').document(name)
+#--
+st.sidebar.header("Nueva película")
 
-submit = st.button('Crear nueva película')
+name = st.sidebar.text_input('Nombre de la película')
+filemcompany = st.sidebar.text_input('Compañía Cinematográfica')
+director = st.sidebar.text_input('Director')
+genre = st.sidebar.text_input('Género')
+
+submit = st.sidebar.button('Crear nueva película')
 
 if name and filemcompany and director and genre and submit:
-  doc_ref = db.collection('movies').document(name)
-  doc_ref.set({
-      'name':name,
-      'filemcompany':filemcompany,
-      'director':director,
-      'genre':genre
-  })
-  st.sidebar.write('Película agregada')
-
+    doc_ref = dbMovies.document(name)
+    doc_ref.set({
+        'name': name,
+        'filemcompany': filemcompany,
+        'director': director,
+        'genre': genre
+    })
+    st.sidebar.success('Película agregada')
+#--
 # Función obtener directores únicos
 def getDirectors():
     docs = dbMovies.stream()
@@ -65,18 +119,6 @@ else:
 
 btnFiltrar = st.sidebar.button("Filtrar")
 
-#--
-#if btnFiltrar and DirectorSearch:
-#    docs = loadByDirector(DirectorSearch)
-#
-#    if len(docs) == 0:
-#        st.sidebar.write("No existen películas del director en la base de datos")
-#    else:
-#        st.sidebar.write(f"Películas de {DirectorSearch}:")
-#        for doc in docs:
-#            st.sidebar.write(doc.to_dict())
-#---
-
 filtered_movies = []
 
 if btnFiltrar and DirectorSearch:
@@ -90,60 +132,59 @@ if btnFiltrar and DirectorSearch:
             filtered_movies.append(doc.to_dict())
 
 
-#Eliminar
-st.sidebar.markdown("______________________________")
-btnEliminar = st.sidebar.button("Eliminar")
+##Eliminar
+#st.sidebar.markdown("______________________________")
+#btnEliminar = st.sidebar.button("Eliminar")
 
-if (btnEliminar):
-  deletename = loadByName(nameSearch)
-  if deletename is None:
-    st.sidebar.write("La película no existe en nuestra base de datos")
-  else:
-    dbMovies.document(deletename.id).delete()
-    st.sidebar.write("Película eliminada")
+#if (btnEliminar):
+#  deletename = loadByName(nameSearch)
+#  if deletename is None:
+#    st.sidebar.write("La película no existe en nuestra base de datos")
+#  else:
+#    dbMovies.document(deletename.id).delete()
+#    st.sidebar.write("Película eliminada")
 
-st.sidebar.markdown("______________________________")
+#st.sidebar.markdown("______________________________")
 
-#Modificar
-newname = st.sidebar.text_input("Modificar nombre")
-btnModificar = st.sidebar.button("Modificar")
+##Modificar
+#newname = st.sidebar.text_input("Modificar nombre")
+#btnModificar = st.sidebar.button("Modificar")
 
-if btnModificar:
-  updatename = loadByName(nameSearch)
-  if updatename is None:
-    st.sidebar.write("La película no existe en nuestra base de datos")
-  else:
-    myupdatename=dbMovies.document(updatename.id)
-    myupdatename.update({
-        'name': newname
-    })
+#if btnModificar:
+#  updatename = loadByName(nameSearch)
+#  if updatename is None:
+#    st.sidebar.write("La película no existe en nuestra base de datos")
+#  else:
+#    myupdatename=dbMovies.document(updatename.id)
+#    myupdatename.update({
+#        'name': newname
+#    })
 
 
 ## Listado  de películas
 #st.markdown("______________________________")
-#st.subheader("Listado completo de películas")
-#
-#movies_ref = list(dbMovies.stream())
-#movies_dict = list(map(lambda x: x.to_dict(), movies_ref))
-#
-#if len(movies_dict) > 0:
-#    movies_dataframe = pd.DataFrame(movies_dict)
-#    st.dataframe(movies_dataframe)
-#else:
-#    st.write("No hay películas registradas")
+#st.subheader("Listado de películas")
 
+#if btnFiltrar and DirectorSearch and len(filtered_movies) > 0:
+#    movies_dataframe = pd.DataFrame(filtered_movies)
+#else:
+#    movies_ref = list(dbMovies.stream())
+#    movies_dict = [doc.to_dict() for doc in movies_ref]
+
+##    movies_dict = list(map(lambda x: x.to_dict(), movies_ref))
+#    movies_dataframe = pd.DataFrame(movies_dict)
+#
+#if not movies_dataframe.empty:
+#    st.dataframe(movies_dataframe, use_container_width=True)
+#else:
+#   st.write("No hay películas registradas")
+
+
+#--------aqui voy
 st.markdown("______________________________")
 st.subheader("Listado de películas")
 
-if btnFiltrar and DirectorSearch and len(filtered_movies) > 0:
-    movies_dataframe = pd.DataFrame(filtered_movies)
-else:
-    movies_ref = list(dbMovies.stream())
-    movies_dict = list(map(lambda x: x.to_dict(), movies_ref))
-    movies_dataframe = pd.DataFrame(movies_dict)
-
-if not movies_dataframe.empty:
-    st.dataframe(movies_dataframe, use_container_width=True)
+if not movies_df.empty:
+    st.dataframe(movies_df, use_container_width=True)
 else:
     st.write("No hay películas registradas")
-    
